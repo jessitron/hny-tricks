db3808d2aa3528c5aab2c0081fb21b3a8f9b7af0
Jessica Kerr <jessitron@gmail.com>
2024-07-04 14:10:36 -0500

    be typescripty

--- src/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const greet = "Hello wrold";
-console.log(greet);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
 const greet: string = "Hello wrold"
 
 console.log(greet);
