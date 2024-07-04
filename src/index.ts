bb8856f7f7bd1a89013df8393581e4d28a290b41
Jessica Kerr <jessitron@gmail.com>
2024-07-04 14:15:57 -0500

    Minimal express app

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  console.log("here we are at /")
+  res.send("Welcome to Jessitron's Honeycomb Tricks");
+})
+
 const greet: string = "Hello wrold"
 
 console.log(greet);
