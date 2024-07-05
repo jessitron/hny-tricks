c5f93785d3fa5375c15ed1cc6397a20df15970c0
Jessica Kerr <jessitron@gmail.com>
2024-07-05 14:51:28 -0500

    I can get an obvious error

--- src/htm-but-right.ts
+++ src/htm-but-right.ts
@@ -3,11 +3,15 @@ import vhtml from "vhtml";
 
 /**
  * htm + vhtml convert template strings into HTML but with JSX-style property and component expansion.
- * 
+ *
  * However, it is shit in error cases, super inscrutable, and I want to make that more obvious.
  */
 const htmlOriginal = htm.bind(vhtml);
 
 export const html = (strings: TemplateStringsArray, ...values: any[]) => {
-    return htmlOriginal(strings, ...values);
-}
+  const result = htmlOriginal(strings, ...values);
+  if (typeof result === "string") {
+    return result;
+  }
+  return `<div style='color:red'>Invalid HTML detected. Did you forget to close a tag?<hr/>${result}<hr/></div>`;
+};
--- test/htm-but-right.test.js
+++ test/htm-but-right.test.js
@@ -1,9 +1,16 @@
 const { html } = require("../src/htm-but-right");
 
 describe("making htm work better in error cases", () => {
-  it("when I forget to close a tag", () => {
-    console.log(html);
+  it("when I forget to close a tag, gives me a recognizable error", () => {
     const result = html`<div><span>hello</div>`;
+    //  expect(result).toBe("<div><span>hello</span></div>"); // this is what htm gives back
+    expect(result).toBe(
+      "<div style='color:red'>Invalid HTML detected. Did you forget to close a tag?<hr/>div,<span>hello</span><hr/></div>"
+    );
+  });
+
+  it("when the html is fine, returns a string", () => {
+    const result = html`<div><span>hello</span></div>`;
     expect(result).toBe("<div><span>hello</span></div>");
   });
 });
