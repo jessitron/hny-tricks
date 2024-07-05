46cb6156d7360dfa84d0dee211d078fa333f3029
Jessica Kerr <jessitron@gmail.com>
2024-07-05 14:52:24 -0500

    Now it is happy

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   console.log("here we are at /");
   const element = html`<html>
     <body>
-      <h1>Welcome to Jessitron's Honeycomb Tricks
+      <h1>Welcome to Jessitron's Honeycomb Tricks</h1>
     </body>
   </html>`;
   console.log(element);
