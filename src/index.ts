0eace27f8ad5565814a218c5ea4b5df91ab190cb
Jessica Kerr <jessitron@gmail.com>
2024-07-05 16:48:06 -0500

    htm + vhtml are indeed too simple
    they insist on html-encoding the JS characters

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   console.log("here we are at /");
   const element = html`<html>
+    <head>
+      <script src="https://unpkg.com/@jessitronica/hny-otel-web@0.2.0/dist/hny.js"></script>
+      ${javascriptToStartTracing}
+    </head>
     <body>
       <h1>Welcome to Jessitron's Honeycomb Tricks</h1>
     </body>
@@ -19,3 +23,13 @@ app.get("/", (req, res) => {
   console.log(element);
   res.send(`<!DOCTYPE html>${element}`);
 });
+
+const javascriptToStartTracing = `<script>
+  console.log("I am the script yo");
+  Hny({
+    apiKey:
+      "hcaik_01j229z3ezkranjdcgj7e5dvx4vz4prbbkkbgx4rnv4strm0kb485sae9f",
+    serviceName: "hny-tricks-web",
+    debug: true,
+  });
+</script>`;
