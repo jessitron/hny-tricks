9a2bb4fd983c32a7a051fd1bf6d81013f73a7443
Jessica Kerr <jessitron@gmail.com>
2024-07-05 20:32:06 -0500

    Styles
    and add the body of the response to the trace. Why not

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import "./tracing";
 import express, { Request, Response } from "express";
 import { html } from "./htm-but-right";
+import { trace } from "@opentelemetry/api";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// serve files from the public directory
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
@@ -18,20 +22,21 @@ app.get("/", (req, res) => {
       <script
         dangerouslySetInnerHTML=${{ __html: javascriptToStartTracing }}
       ></script>
+      <title>Hny Tricks</title>
+      <link rel="stylesheet" href="styles.css" />
     </head>
     <body>
-      <h1>Welcome to Jessitron's Honeycomb Tricks</h1>
+      <h1>Jessitron's Honeycomb Tricks</h1>
     </body>
   </html>`;
-  console.log(element);
-  res.send(`<!DOCTYPE html>${element}`);
+  const fullResponse = `<!DOCTYPE html>${element}`;
+  trace.getActiveSpan()?.setAttribute("response.body", fullResponse);
+  res.send(fullResponse);
 });
 
 const javascriptToStartTracing = `
   console.log("I am the script yo");
-   Hny({
-          apiKey:
-            "hcaik_01j229z3ezkranjdcgj7e5dvx4vz4prbbkkbgx4rnv4strm0kb485sae9f",
+   Hny({  apiKey: "hcaik_01j229z3ezkranjdcgj7e5dvx4vz4prbbkkbgx4rnv4strm0kb485sae9f",
           serviceName: "hny-tricks-web",
           debug: true,
         });
