0f8829e53c5997a0bb06448be45ba7d026778cbb
Jessica Kerr <jessitron@gmail.com>
2024-07-07 09:15:22 -0500

    formatting

--- src/HoneycombApiKey.ts
+++ src/HoneycombApiKey.ts
@@ -3,10 +3,14 @@ import { html } from "./htm-but-right";
 export function Team(apiKey: string | undefined): string {
   return html`<div class="team">
     <form hx-post="/team" hx-target="#stuff" id="apikey-form">
+      <div>
         <label for="apikey">Honeycomb API Key:</label>
         <input id="apikey" name="apikey" hx-post="/validate" hx-target="#apikey-opinion" ></input>
         <button>Check Permissions</button>
+      </div> 
+      <div>
         <span id="apikey-opinion"></span>
+      </div>
     </form>
     <p class="fine-print">This API key will be sent to the Honeycomb Tricks backend, but we don't save it. We call the Honeycomb auth endpoint,
     and then tell you which team and environment it belongs to, and which permissions it has.
