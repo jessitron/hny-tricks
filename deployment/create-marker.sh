5acdc9b1ed47d819068dcf76b6b539a061d25edc
Jessica Kerr <jessitron@gmail.com>
2024-07-17 22:29:19 -0500

    Make deployment script work
    ok it's live at https://util.jessitron.honeydemo.io

---
+++ deployment/create-marker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ref=$(git rev-parse HEAD | cut -c1-10)
+if [[ -z "$VERSION_NUMBER" ]]; then
+    message="ref:$ref"
+else
+    message="v$VERSION_NUMBER $1 ref:$ref"
+fi
+body=$(echo '{"message":"'$message'", "type":"deploy"}')
+
+if [[ -n "$1" ]]; then
+    dataset="$1-nodejs" # this is the OTEL_SERVICE_NAME from docker-compose.yml or k8s.yaml for the service of name $1. We happen to have a convention, it won't work for 'web'
+else
+  dataset="__all__"
+fi
+
+echo "dataset: $dataset"
+
+curl https://api.honeycomb.io/1/markers/${dataset} -X POST  \
+    -H "X-Honeycomb-Team: ${HONEYCOMB_API_KEY}"  \
+    -d "$body"
