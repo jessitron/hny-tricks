cd577dd4d8b94992c17975993daec9341d5aa2b4
Jessica Kerr <jessitron@gmail.com>
2024-08-25 18:15:17 -0500

    New versions of my own forks

---
+++ public/update-my-libs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# run this on my (jessitron's) computer, where I have these other projects that I'm playing with
+set -e
+set -x
+
+cp ~/code/jessitron/hny-otel-web/dist/hny.js .
+cp ~/code/bigskysoftware/htmx/dist/htmx.js .
