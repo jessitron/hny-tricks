518c26f821597a1a6cac1da65c3ebcecb7430c65
Jessica Kerr <jessitron@gmail.com>
2025-01-10 22:17:50 -0600

    test: identify the condition that makes it sad

--- test/htm-exploratory.test.ts
+++ test/htm-exploratory.test.ts
@@ -15,10 +15,18 @@ describe("the htm+vhtml library", () => {
     expect(actual).toBe(expected);
   });
 
-  it("sometimes does not escape html when inserted as a value", () => {
+  it("does not escape html when one element is inserted as a value", () => {
     const inner = html`<p>stuff</p>`;
     const outer = html`<div>${inner}</div>`;
     const expected = `<div><p>stuff</p></div>`;
+    expect(outer).toBe(expected);
+  });
+
+  it("escapes html when it is more than one element", () => {
+    const inner1 = html`<p>stuff</p>` as string;
+    const inner2 = html`<p>more stuff</p>` as string;
+    const outer = html`<div>${inner1 + inner2}</div>`;
+    const expected = `<div>&lt;p&gt;stuff&lt;/p&gt;&lt;p&gt;more stuff&lt;/p&gt;</div>`;
     expect(outer).toBe(expected);
   });
 
