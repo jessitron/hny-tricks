24ba39aaba8175967bdb54e76f73a28c5b835477
Jessica Kerr <jessitron@gmail.com>
2025-01-10 22:10:33 -0600

    finally duplicated

--- test/htm-exploratory.test.ts
+++ test/htm-exploratory.test.ts
@@ -14,4 +14,35 @@ describe("the htm+vhtml library", () => {
     const expected = `<div>stuff</div>`;
     expect(actual).toBe(expected);
   });
+
+  it("sometimes does not escape html when inserted as a value", () => {
+    const inner = html`<p>stuff</p>`;
+    const outer = html`<div>${inner}</div>`;
+    const expected = `<div><p>stuff</p></div>`;
+    expect(outer).toBe(expected);
+  });
+
+  it("sometimes does escape html when inserted as a value", () => {
+    const currentTraceId = "s;aofdjhaldkjfhalskjdhf";
+    const status = {
+      success: true,
+      html: [
+        html`<p class="success-result">
+          Column dc.dataset created in observaquiz-browser 🙂
+        </p>`,
+        html`<p class="success-result">
+          Column dc.dataset created in observaquiz-bff 🙂
+        </p>`,
+      ].join(" "),
+    };
+    const actual = html`<div
+      data-traceid=${currentTraceId}
+      class="status ${status.success ? "happy" : "unhappy"}"
+    >
+      ${status.html}
+    </div>`;
+
+    const expected = `<div data-traceid=\"s;aofdjhaldkjfhalskjdhf\" class=\"status happy\">&lt;p class=&quot;success-result&quot;&gt;Column dc.dataset created in observaquiz-browser 🙂&lt;/p&gt; &lt;p class=&quot;success-result&quot;&gt;Column dc.dataset created in observaquiz-bff 🙂&lt;/p&gt;</div>`;
+    expect(actual).toBe(expected);
+  });
 });
